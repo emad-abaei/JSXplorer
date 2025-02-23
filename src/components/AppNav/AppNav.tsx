c7d61ba2c8c0e02fb74b92443551d933faec83c3
Emad Abaei <emad.ae14@gmail.com>
2025-02-23 16:03:33 +0330

    feat(AppNav): imporve accibility

--- src/components/AppNav/AppNav.tsx
+++ src/components/AppNav/AppNav.tsx
@@ -3,13 +3,17 @@ import styles from "./AppNav.module.css";
 
 function AppNav() {
   return (
-    <nav className={styles.nav}>
+    <nav className={styles.nav} role='navigation' aria-label='App Navigation'>
       <ul>
         <li>
-          <NavLink to='cities'>Cities</NavLink>
+          <NavLink to='cities' aria-label='View cities list'>
+            Cities
+          </NavLink>
         </li>
         <li>
-          <NavLink to='countries'>Countries</NavLink>
+          <NavLink to='countries' aria-label='View countries list'>
+            Countries
+          </NavLink>
         </li>
       </ul>
     </nav>
