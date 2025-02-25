38d92692482d1ff746df0b952b066799cd8a477d
Emad Abaei <emad.ae14@gmail.com>
2025-02-26 00:11:20 +0330

    feat(Spinner): imporve accibility

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -2,8 +2,9 @@ import styles from "./Spinner.module.css";
 
 function Spinner() {
   return (
-    <div className={styles.spinnerContainer}>
+    <div className={styles.spinnerContainer} role='status' aria-live='polite'>
       <div className={styles.spinner}></div>
+      <span className='sr-only'>Loading...</span>
     </div>
   );
 }
