22ecdbe4e45dd1238daf3e62338d4e57965f6c18
Emad Abaei <emad.ae14@gmail.com>
2025-02-26 00:01:32 +0330

    feat(ErrorFallback): imporve accibility

--- src/components/ErrorFallback/ErrorFallback.tsx
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -4,8 +4,8 @@ import styles from "./ErrorFallback.module.css";
 
 function ErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
   return (
-    <div role='alert' className={styles.error}>
-      <h2>Something went wrong</h2>
+    <div role='alert' aria-live='assertive' className={styles.error}>
+      <h1>Something went wrong</h1>
       <p>{error.message}</p>
       <Button onClick={resetErrorBoundary}>Try Again</Button>
     </div>
